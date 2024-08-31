85b631770a905fb1c40110dac75224143a1eca9e
HCarbia <heriberto.carbia@upr.edu>
2024-08-31 14:50:30 -0400

    e

--- add.cpp
+++ add.cpp
@@ -9,6 +9,6 @@ int main() {
   number2 = 13;
   sum = number1 + number2;
   std::cout << "The sum of these two integers is "
-            << sum << std::endl
-  return 0;}
+            << sum << std::endl;
+  return 0}
 
