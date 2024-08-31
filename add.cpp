79b93c59abef5e704d96626b8011383bf4d9ca50
HCarbia <heriberto.carbia@upr.edu>
2024-08-31 14:48:28 -0400

    e

--- add.cpp
+++ add.cpp
@@ -4,11 +4,11 @@
 #include <iostream>
 
 int main() {
-  int number1, number2, sum
+  int number1, number2, sum:
   number1 = 12;
   number2 = 13;
   sum = number1 + number2;
   std::cout << "The sum of these two integers is "
-            << sum << std::endl;
+            << sum << std::endl
   return 0;}
 
